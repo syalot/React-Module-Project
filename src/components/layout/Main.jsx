abb553a507993aa23c6a2f6a295a90c23b72b9ff
syalot <syalot506@gmail.com>
2016-11-19 14:35:34 +0900

    Create(*): 메인페이지 및 서브페이지 생성

---
+++ src/components/layout/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Main extends React.Component{
+  render(){
+    return(
+      <div>
+        <h2>Main</h2>
+      </div>
+    );
+  }
+};
+
+export default Main;
